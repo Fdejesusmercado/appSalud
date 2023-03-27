8fa70717af5048e3ecfdff54a60f6501f4c2fae6
Fabio <fdejesusmercado@unibarranquila.edu.co>
2023-03-26 22:58:20 -0500

    Mejorando APPsalud

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import {LoginService} from '../../app/login.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Location } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -10,13 +12,17 @@ import { CookieService } from 'ngx-cookie-service';
 export class PerfilComponent implements OnInit {
 
   nombrePerfil = ''
+  documentosQllegan :any 
+  discapacidadesQllegan:any
+  DiscapacidadesAeliminar:any = []
   ngOnInit(): void {
     this.cargarPerfil()
     
     throw new Error('Method not implemented.');
   }
+  
 
-  constructor(private servicio: LoginService,private cookieService: CookieService){
+  constructor(private msg:ToastrService,router : Router,private loca:Location,private servicio: LoginService,private cookieService: CookieService){
 
   }
 
@@ -25,9 +31,40 @@ export class PerfilComponent implements OnInit {
       if (R){
         
         this.nombrePerfil = R.fullname
+        
         console.log(R) 
+        
+        this.documentosQllegan = R.documentos
+        this.discapacidadesQllegan = R.discapacidades
+        console.log(this.documentosQllegan) 
       }
     })
     
+  }
+
+  remover(idElemento:any){
+    
+    const elemento =  document.getElementById(`${idElemento}`)
+    elemento?.remove()
+    console.log(elemento)
+    this.DiscapacidadesAeliminar.push(idElemento)
+    console.log(this.DiscapacidadesAeliminar)
+  }
+  
+
+  eliminarDiscapacidad(){
+    this.servicio.eliminarDiscapacidad({'token':this.cookieService.get('loginToken'),'dis_eliminar':this.DiscapacidadesAeliminar}).subscribe(R=>{
+      if (R){
+        console.log(R)
+        this.msg.error(`Aplicando los cambios`, 'Alerta', {
+          timeOut: 2000,
+          progressBar:true,
+          });
+        setTimeout(()=>{
+          window.location.reload();
+        },2000)
+       
+      }
+    })
   }
 }
